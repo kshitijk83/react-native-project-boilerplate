22dfd412bb5c9711fa0a52da3fdc41e0beb609b0
kshitijk83 <kshitijk83@gmail.com>
2019-07-16 15:16:12 +0530

    react debugger

--- boilerplate/src/store/index.js
+++ boilerplate/src/store/index.js
@@ -1,22 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import reducer from '../reducers';
 
 const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__ })
 
-// const store = createStore(
-//     reducer /* preloadedState, */,
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-//   )
-
 function configureStore(initialState) {
     const enhancer = compose(
-        applyMiddleware(
+        composeWithDevTools(applyMiddleware(
             thunkMiddleware,
             loggerMiddleware,
-        ),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        ))
     );
     return createStore(reducer, initialState, enhancer)
 }
